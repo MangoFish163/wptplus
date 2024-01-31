c97b60b3a1e2e78302456908c0b43000ca8899d4
Mango Fish <mangofish_cc@163.com>
2024-01-31 10:51:53 +0800

    UP Test

--- test.php
+++ test.php
@@ -3,13 +3,16 @@
  * @wordpress-plugin
  * Plugin Name:         Autolode
  * Description:         在线更新功能测试
- * Version:             0.0.3
+ * Version:             0.0.4
  * Author:              Taotens
 */
 
-if(!defined('AUTOLODE_DIR'))
-{
-	define( 'AUTOLODE_DIR', plugin_dir_path( __FILE__ ) );
+if (!defined('AUTOLODE_DIR')) {
+    define('AUTOLODE_DIR', plugin_dir_path(__FILE__));
+
+    include_once ABSPATH . 'wp-admin/includes/plugin.php';
+    define('AUTOLODE_DATA', get_plugin_data(__FILE__));
+    define('AUTOLODE_DI_NAME', basename(AUTOLODE_DIR));
 }
 
 function activate_Wp_Autolode_Plugin()
@@ -23,8 +26,9 @@ try {
     register_deactivation_hook(__FILE__, 'deactivate_Wp_Autolode_Plugin');
 } catch (\Throwable $th) {
 }
-require_once(plugin_dir_path(__FILE__).'/upload.php');
+require_once(plugin_dir_path(__FILE__) . '/upload.php');
+
 /**
-* Load the automatic check updater
-*/
+ * 加载自动检查更新程序
+ */
 if (is_admin()) new UpdateChecker(plugin_basename(__FILE__));
--- upload.php
+++ upload.php
@@ -1,6 +1,6 @@
 <?php
 /*
-* 这个简单的类只负责检查和更新当前插件
+* 负责检查和更新当前插件
 * Author: Mango Fish
 */
 
@@ -10,7 +10,7 @@ if (!class_exists('UpdateChecker')) {
         public $plugin_slug;
         public $version;
         public $cache_key;
-        public $cache_allowed;
+        public $interval;
         public $updateNum = 0;
         public $plugin_entrance;
         public $timeout;
@@ -22,19 +22,19 @@ if (!class_exists('UpdateChecker')) {
             if (!is_admin()) return false;
             $bool = is_array($init);
             $this->plugin_slug = $bool ? $init['plugin_slug'] : $init;
-            // 标识(远程获取更新信息，本地缓存使用)
+            // 标识
             $this->cache_key = $bool ? $init['cache_key'] : $this->plugin_slug;
-            // 缓存超时时长
+            $this->interval = $bool && isset($init['cache_key'])? $init['cache_key'] : false;
+            // 缓存时长
             $this->timeout = $bool ? $init['timeout'] : 60;
-            // 启用缓存
-            $this->cache_allowed = true;
             // 获取当前插件版本号
-            $this->version = get_option($this->plugin_slug . '_version');
+            $this->version = AUTOLODE_DATA['Version'];
             // 插件入口文件
-            $this->plugin_entrance = $this->plugin_slug . '/' . $this->plugin_slug . '.php';
-            add_filter('site_transient_update_plugins', array($this, 'check_for_plugin_update'));
-            add_filter('upgrader_source_selection', array($this, 'custom_upgrade_directory'), 10, 3);
-
+            $this->plugin_entrance = $this->plugin_slug;
+            if ($this->cache_key == false || get_transient($this->cache_key) === false) {
+                add_filter('site_transient_update_plugins', array($this, 'check_for_plugin_update'));
+                add_filter('upgrader_source_selection', array($this, 'custom_upgrade_directory'), 10, 3);
+            }
         }
 
         public function check_for_plugin_update($transient)
@@ -43,11 +43,14 @@ if (!class_exists('UpdateChecker')) {
                 return $transient;
             }
             if (self::$responseBody) {
-                $transient->response[$this->plugin_slug] = self::$responseBody;
+                if (version_compare($transient->checked[$this->plugin_entrance], self::$responseBody->new_version, '<')) {
+                    $transient->response[$this->plugin_slug] = self::$responseBody;
+                };
                 return $transient;
-            } // 向 GitHub API 发送请求，检查是否有新版本
+            }
+            // 向 GitHub API 发送请求，检查是否有新版本 地址/token换成自己的就行
             $github_api_url = 'https://api.github.com/repos/MangoFish163/wptplus/releases/latest';
-            $personal_access_token = json_decode(file_get_contents(AUTOLODE_DIR . '../../text-config.json'))->token;
+            $personal_access_token = json_decode(file_get_contents(WP_CONTENT_DIR . '/text-config.json'))->token;
             $Agents = array(
                 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.71 Safari/537.1 LBBROWSER',
                 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E)',
@@ -59,7 +62,7 @@ if (!class_exists('UpdateChecker')) {
             $headers = array(
                 'Authorization: Bearer ' . $personal_access_token,
                 'Accept: application/vnd.github+json',
-                'User-Agent: ' . $Agents[array_rand($Agents)], // 替换为您自己的 User Agent 名称
+                'User-Agent: ' . $Agents[array_rand($Agents)],
             );
             $response = wp_remote_get($github_api_url, array('headers' => $headers));
             $body = wp_remote_retrieve_body($response);
@@ -67,48 +70,53 @@ if (!class_exists('UpdateChecker')) {
             if ($body) {
                 $release_info = json_decode($body, true);
                 $latest_version = $release_info['tag_name'];
-                // 检查最新版本与当前版本的比较
+                // 检查最新版本与当前版本的比较 设置更新信息
+                $obj = new stdClass();
+                $obj->slug = $this->plugin_slug;
+                $obj->new_version = $latest_version ?? 0;
+                $obj->url = $release_info['html_url'];
+                $obj->package = $release_info['zipball_url'];
+                self::$responseBody = $obj;
                 if (version_compare($transient->checked[$this->plugin_entrance], $latest_version, '<')) {
-                    // 有新版本可用，设置更新信息
-                    $obj = new stdClass();
-                    $obj->slug = $this->plugin_slug;
-                    $obj->new_version = $latest_version;
-                    $obj->url = $release_info['html_url'];
-                    $obj->package = $release_info['zipball_url'];
                     $transient->response[$obj->slug] = $obj;
-                    self::$responseBody = $obj;
                 }
             }
-            file_put_contents(AUTOLODE_DIR . '/look.json', json_encode(WP_PLUGIN_DIR ), FILE_APPEND);
+
             return $transient;
         }
         // 过滤器回调函数
         public function custom_upgrade_directory($source, $remote_source, $upgrader)
         {
-            // 检查是否是由我们的插件触发的更新
             if (strpos($source, 'MangoFish163') !== false) {
-                $target_directory = WP_PLUGIN_DIR . '/tets';
-                
-                if (!file_exists($target_directory)) {
-                    mkdir($target_directory, 0755, true);
-                }
-
-                return $target_directory;
+                $target_directory = WP_CONTENT_DIR . "/upgrade/" . AUTOLODE_DI_NAME . "/";
+                return $this->move_folder_contents($source, $target_directory) ? $target_directory : $source;
+            }
+            self::$responseBody = false;
+            if($this->interval){
+                set_transient( $this->cache_key, $this->version, $this->timeout );
             }
             return $source;
+        }
+        // 移动文件到指定目录,避免默认移动到官方默认去重目录
+        public function move_folder_contents($source, $destination)
+        {
+            if (!file_exists($source)) {
+                return false;
+            }
+            if (!file_exists($destination)) {
+                mkdir($destination, 0755, true);
+            }
+            $items = glob($source . '/*');
+            foreach ($items as $item) {
+                $new_item = $destination . '/' . basename($item);
+                rename($item, $new_item);
+            }
+            return true;
         }
 
         public function purge($upgrader, $options)
         {
-
-            if (
-                $this->cache_allowed
-                && 'update' === $options['action']
-                && 'plugin' === $options['type']
-            ) {
-                // 当安装新插件版本时，清理缓存
-                delete_transient($this->cache_key);
-            }
+            
         }
     }
 }
