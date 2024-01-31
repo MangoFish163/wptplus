7454fa42f1f16a05285a4f83d8ba85c50af885c6
Mango Fish <mangofish_cc@163.com>
2024-01-31 16:31:21 +0800

    UP autolode

--- autoload.php
+++ autoload.php
@@ -1,25 +1,21 @@
 <?php
 /*
  * @wordpress-plugin
- * Plugin Name:         Autolode
+ * Plugin Name:         UpdateChecker
  * Description:         在线更新功能
  * Version:             0.0.5
  * Author:              Mango Fish
 */
 
-if (!defined('AUTOLODE_DIR')) {
-    define('AUTOLODE_DIR', plugin_dir_path(__FILE__));
-}
-
-function activate_Wp_Autolode_Plugin()
+function activate_Wp_UpdateChecker_Plugin()
 {
 }
-function deactivate_Wp_Autolode_Plugin()
+function deactivate_Wp_UpdateChecker_Plugin()
 {
 }
 try {
-    register_activation_hook(__FILE__, 'activate_Wp_Autolode_Plugin');
-    register_deactivation_hook(__FILE__, 'deactivate_Wp_Autolode_Plugin');
+    register_activation_hook(__FILE__, 'activate_Wp_UpdateChecker_Plugin');
+    register_deactivation_hook(__FILE__, 'deactivate_Wp_UpdateChecker_Plugin');
 } catch (\Throwable $th) {
 }
 
@@ -27,7 +23,7 @@ try {
 * 加载自动检查更新程序
 */
 if (!defined('UPDATECHECKER_DATA')) {
-    require_once(plugin_dir_path(__FILE__) . '/upload.php');
+    require_once(plugin_dir_path(__FILE__) . '/update-checker.php');
     require_once(ABSPATH . 'wp-admin/includes/plugin.php');
     define('UPDATECHECKER_DATA', get_plugin_data(__FILE__));
     define('UPDATECHECKER_DIR_NAME', basename(plugin_dir_path(__FILE__)));
